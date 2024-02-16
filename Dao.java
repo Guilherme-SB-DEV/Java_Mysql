46aa4ea9c1ded5d0178d9b11c7792d4108d1d901
Gui_S.B <141243910+Guilherme-SB-DEV@users.noreply.github.com>
2024-02-15 21:06:19 -0300

    Update Dao.java

--- Dao.java
+++ Dao.java
@@ -1,35 +1,71 @@
-package mysql;
-import java.sql.CallableStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+package sqldodemonio;
 
+import java.sql.*;
+import static sqldodemonio.Conexao.open;
 
-public class Dao {
-    Connection conexao;
-    PreparedStatement prepared;
-    ResultSet rs;
-    CallableStatement call;
-    
-    
-    public void open() throws Exception{
-        String usuario = "#####";
-        String senha = "#####";
-        String url = "jdbc:mysql://localhost:3306/sla";
-        String dri = "com.mysql.jdbc.Driver";
-        try{
-            Class.forName(dri);
-            conexao = DriverManager.getConnection(url, usuario, senha);
-        }catch(Exception ex){
-            ex.printStackTrace(); 
+public class DAO {
+    public static String instrucao_select = "SELECT * FROM new_table;";
+    public static String instrucao_insert = "INSERT INTO new_table(nome, idade) VALUES (?,?);";
+    public static String instrucao_update = "UPDATE new_table SET nome=?, idade=? WHERE id =?;";
+    public static String instrucao_delete = "DELETE FROM new_table WHERE id = ?;";
+        public static void consulta(){
+            
+            try{
+                Connection con = open();
+                Statement sentenca = con.createStatement();
+                ResultSet rs = sentenca.executeQuery(instrucao_select);
+                while(rs.next()){
+                    int id = rs.getInt("id");
+                    String nome = rs.getString("nome");
+                    int idade = rs.getInt("idade");
+                    System.out.println("id: "+id+" nome: "+nome+" idade: "+idade);
+                }
+            }
+            catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
         }
-    }
-    public void close() throws Exception{
-        conexao.close();
-    }
-    
+        public static Boolean insert(String nome, int idade){
+            try{
+                Connection con = open();
+                PreparedStatement stm = con.prepareStatement(instrucao_insert);
+                stm.setString(1, nome);
+                stm.setInt(2, idade);
+                stm.execute();
+            return true;
+            }catch(Exception ex){
+                System.out.println(ex.getMessage());
+                return false;
+            }
+        }
+        public static Boolean update(int id, String new_nome, int new_idade){
+            try {
+                Connection con = open();
+                PreparedStatement stm = con.prepareStatement(instrucao_update);
+                stm.setString(1, new_nome);
+                stm.setInt(2, new_idade);
+                stm.setInt(3, id);
+                stm.execute();
+                
+                return true;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return false;
+            }
+            
+        }
+        public static Boolean delete(int id){
+            try {
+                Connection con = open();
+                PreparedStatement stm = con.prepareStatement(instrucao_delete);
+                stm.setInt(1, id);
+                stm.execute();
+                        
+                return true;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return false;
+            }
+        }
 
 }
