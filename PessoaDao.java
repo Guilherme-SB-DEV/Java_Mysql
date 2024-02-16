ce34bc9feeace258669fe4159f5a209c322bf490
Gui_S.B <141243910+Guilherme-SB-DEV@users.noreply.github.com>
2024-02-15 21:08:31 -0300

    PessoaDao.java

--- PessoaDao.java
+++ PessoaDao.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package mysql;
+package sql;
 
-public class PessoaDao extends Dao{
-    String instrucao_insert = "INSERT INTO pessoa(nome, email, imc) VALUES (?,?,?)";
-    String instrucao_delete = "DELETE FROM pessoa WHERE idpessoa = ?";
-    String instrucao_update = "UPDATE pessoa SET nome = ?, email = ?, imc = ? WHERE idpessoa = ?";
-    
-    
+public class PessoaDao extends DAO{
+    private String nome;
+    private int idade;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
 }
