9cc3e4136d6a8a0f886d5e3a8f6feec7e1e9af7d
Gui_S.B <141243910+Guilherme-SB-DEV@users.noreply.github.com>
2024-02-15 21:13:58 -0300

    Update and rename Mysql.java to Sql.java

---
+++ Sql.java
@@ -0,0 +1,11 @@
+package sql;
+import java.util.Scanner;
+import sql.DAO;
+public class Sql extends PessoaDao{
+
+    public static void main(String[] args){
+        // TODO code application logic here
+        PessoaDao p1 = new PessoaDao();
+    }
+    
+}
