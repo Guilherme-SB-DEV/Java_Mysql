eea218f37cd3825e840ee3b67cd1e03211aa1369
Gui_S.B <141243910+Guilherme-SB-DEV@users.noreply.github.com>
2024-02-15 21:11:23 -0300

    Conexao.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,41 +1,29 @@
-
-package mysql;
-
-public class Pessoa {
-    private String nome;
-    private String email;
-    private Double imc;
+package sql;
+import java.sql.*;
+public class Conexao {
+    
+    static Connection conexao;
+    PreparedStatement prepared;
+    ResultSet rs;
+    CallableStatement call;
+    
+    public static Connection open() throws Exception{
+        String usuario = "root";
+        String senha = "Gui0205#";
+        String url = "jdbc:mysql://localhost:3306/node_db";
+        String dri = "com.mysql.jdbc.Driver";
+        try{
+            Class.forName(dri);
+            conexao = DriverManager.getConnection(url, usuario, senha);
+        }catch(Exception ex){
+            ex.printStackTrace(); 
+        }
+       
+        return conexao;
+    }
+    public void close() throws Exception{
+        conexao.close();
+    }
     
 
-    /**
-     * @return the nome
-     */
-    public String getNome() {
-        return nome;
-    }
-
-    /**
-     * @param nome the nome to set
-     */
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    Double calcImc(Double peso, Double altura){
-        return peso/altura;
-    }
 }
